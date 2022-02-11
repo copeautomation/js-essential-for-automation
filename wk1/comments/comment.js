2826b0b42e1519440ce1bca09227d850f4586af3
Cope Automation <copeautomation@gmail.com>
2022-02-11 14:22:55 +1100

    comments

---
+++ wk1/comments/comment.js
@@ -0,0 +1,15 @@
+#! usr/bin/node
+
+// My first program in JS
+console.log("Hello, World!")
+
+
+/**
+ * Adds two numbers
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns sum of two numbers
+ */
+function sum(num1, num2){
+    return num1 + num2 // return type is a number
+}
