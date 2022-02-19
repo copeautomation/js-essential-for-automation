49d2653174a71524261e9f337cb4bdd1cd9386b9
Cope Automation <copeautomation@gmail.com>
2022-02-19 11:21:03 +1100

    strict mode

---
+++ wk2/strict-mode/strictmode.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * Use case:
+ * 1. Prevents any undeclared global variable
+ * 2. Use of implements, interface, let, package, private, protected, public, static, and yield as identifiers
+ * 
+ */
+
+// Use case 1
+// a = 1
+// console.log(a);
+
+// Use case 2:
+
+let package = "any package"
+console.log(package);
